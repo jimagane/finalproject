e857b2f39aa3613b30afdd14cd3d6db0ad24c025
Jane <jane@Janes-MacBook-Pro.local>
2018-11-03 23:59:29 -0700

    feat: show and hide map

--- src/map.js
+++ src/map.js
@@ -2,11 +2,31 @@ import React from 'react';
 
 class Map extends React.Component {
 
+  state = {
+    active: false
+  }
+
+  toggleClass = () => {
+    const current = this.state.active;
+    this.setState({active: !current});
+    let buttonMap = document.getElementById('mapButton');
+    let listvenues = document.getElementById('maincontent');
+    if (buttonMap.innerText === 'Hide Map') {
+      buttonMap.innerText = 'Show Map';
+      buttonMap.style.bottom = '0';
+      listvenues.style.paddingBottom = '2%';
+    } else {
+      buttonMap.innerText = 'Hide Map';
+      buttonMap.style.bottom = '80%';
+      listvenues.style.paddingBottom = '50%';
+    }
+  }
+
   render() {
     return (
       <div>
-        <button id="mapButton" className="showMap">Hide Map</button>
-        <div id="map">
+        <button id="mapButton" onClick={this.toggleClass}>Hide Map</button>
+        <div id="map" className={this.state.active?'hideMap': null}>
           {this.props.clearMarkers(this.props.markers)}
           {this.props.loadMarkers(this.props.map, this.props.largeInfoWindow, this.props.bounds)}
         </div>
