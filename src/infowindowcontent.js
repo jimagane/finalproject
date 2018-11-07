4dde291e07c5f90c3152b9c251bd75e78fca02c0
Jane <jane@Janes-MacBook-Pro.local>
2018-11-06 21:31:06 -0800

    feat: 3rd party

--- src/infowindowcontent.js
+++ src/infowindowcontent.js
@@ -3,11 +3,17 @@ import React from 'react';
 class InfoWindowContent extends React.Component {
 
   state = {
-    active: false,
+    active: true,
     button: 'See Reviews'
   }
 
+  componentDidUpdate() {
+    this.loadYelpEmbedScript();
+  }
+
   showReview = (id) => {
+    this.setId();
+    this.props.updateid();
     const current = this.state.active;
     let reviewsButton = this.state.button;
     if (reviewsButton === 'See Reviews') {
@@ -16,6 +22,26 @@ class InfoWindowContent extends React.Component {
       reviewsButton = 'See Reviews';
     }
     this.setState({active: !current, button: reviewsButton});
+  }
+
+  setId = () => {
+    if (this.props.result.reviews !== undefined && this.props.result.reviews.length > 0) {
+      let id = this.props.result.reviews[0].id;
+      let id2 = this.props.result.reviews[1].id;
+      let id3 = this.props.result.reviews[2].id;
+      return <div><span className="yelp-review" data-review-id={id} data-hostname="www.yelp.com"></span>
+<span className="yelp-review" data-review-id={id2} data-hostname="www.yelp.com"></span>
+<span className="yelp-review" data-review-id={id3} data-hostname="www.yelp.com"></span>
+</div>
+    }
+  }
+
+  loadYelpEmbedScript = () => {
+    let container = document.getElementById('yelpReview');
+    let scriptYelp = document.createElement('script');
+    scriptYelp.src= "https://www.yelp.com/embed/widgets.js";
+    scriptYelp.async= true;
+    container.appendChild(scriptYelp);
   }
 
   render() {
@@ -29,8 +55,8 @@ class InfoWindowContent extends React.Component {
         </div>
         <div id="yelpBox">
           <button id="reviewsButton" ref={this.props.result.id} onClick={this.showReview}>{this.state.button}</button>
-          <div id="yelpReview" className={this.state.active?'showReview': null}>
-            <span className="yelp-review" data-review-id="KNpcEBZXlF3n4C8STFi2vQ" data-hostname="www.yelp.com"></span>
+          <div id="yelpReview" className={this.state.active?'showReview': 'hideReview'}>
+            {this.setId()}
           </div>
         </div>
       </div>
