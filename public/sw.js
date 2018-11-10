7221993c466e631b1a9d4562c462883a0354beb7
Jane <jane@Janes-MacBook-Pro.local>
2018-11-10 11:08:01 -0800

    updates

---
+++ public/sw.js
@@ -0,0 +1,32 @@
+console.log('sw working');
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('venuesdata01').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/ring.jpg',
+        './src/App.css',
+        './src/App.js',
+        './src/index.css',
+        './src/index.js',
+        './src/infowindowcontent.js',
+        './src/list.js',
+        './src/map.js',
+        './src/YelpAPI.js',
+        './node_modules',
+        './package-lock.json',
+        './package.json'
+      ]);
+    }));
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
--- src/index.js
+++ src/index.js
@@ -9,4 +9,14 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.register();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+  .then(function(reg) {
+    console.log('Registration successful');
+  })
+  .catch(function(err) {
+    console.log('Registration failed', err);
+  });
+}
