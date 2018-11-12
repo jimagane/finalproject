1024e07c5fc9408619cc870a1d5f1e95353b538a
Jane <jane@Janes-MacBook-Pro.local>
2018-11-11 18:18:38 -0800

    serviceworker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -9,3 +10,13 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 // serviceWorker.register();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+  .then(function(reg) {
+    console.log('Registration successful');
+  })
+  .catch(function(err) {
+    console.log('Registration failed', err);
+  });
+}
